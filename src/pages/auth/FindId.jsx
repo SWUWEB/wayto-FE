3d01574ef86c352fefcfdc452f35662de69b3dc3
chae1125 <y72682685@gmail.com>
2025-07-17 21:21:42 +0900

    :tada: Chore: 프로젝트 초기 세팅

---
+++ src/pages/auth/FindId.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FindId = () => {
+  return (
+    <div>
+      <h1>아이디 찾기에 오신 것을 환영합니다!</h1>
+      <p>이곳은 기본 FindId 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default FindId;
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>로그인에 오신 것을 환영합니다!</h1>
+      <p>이곳은 기본 Login 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default Login;
